af9c8bca6415db057f59fc5b8021544cc696a6aa
Sid-darth <realsiddarth@gmail.com>
2020-03-18 00:18:32 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,21 @@
+--set required libraries and classes
+
+
+function love.load()
+img = {} --table with sprites listed
+img.background = 
+img.player = 
+imag.enemy = 
+img.projectile = 
+
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
