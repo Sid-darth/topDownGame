5543a2ec051393a718f9748c02a32618a92cec69
Sid-darth <realsiddarth@gmail.com>
2020-03-21 01:15:04 -0700

    set up player sprite and player.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,19 @@
---set required libraries and classes
-
-
 function love.load()
+-- love.window.setMode(700,550)
+love.graphics.setBackgroundColor(0.65,0.1,0.15)
 img = {} --table with sprites listed
-img.background = 
-img.player = 
-imag.enemy = 
-img.projectile = 
+-- img.background = 
+img.player_guard =  love.graphics.newImage("sprites/player_guard.png")
+img.player_fire = love.graphics.newImage("sprites/player_fire.png")
+img.monster_1 = love.graphics.newImage("sprites/monster_1.png")
+img.monster_2 = love.graphics.newImage("sprites/monster_2.png")
+-- imag.enemy = 
+-- img.projectile = 
+
+gameState = 1
+window = {}
+window.width = love.graphics.getWidth() ; window.height = love.graphics.getHeight()
+require('player')
 
 
 end
@@ -16,6 +23,15 @@ function love.update(dt)
 end
 
 function love.draw()
+	love.graphics.draw(img.player_guard, window.width/2, window.height/2, 
+		nil,1/20,1/20,player.width/2, player.height/2)
+	love.graphics.print("W, H:".. window.width.. ",".. window.height)
 
+end
+
+function love.keypressed(key, scancode, isrepeat)
+	if key == 'escape' then
+		love.event.quit()
+	end
 end
 
---
+++ player.lua
@@ -0,0 +1,5 @@
+player = {}
+
+player.x = love.graphics.getWidth()/2 ; player.y = love.graphics.getHeight()/2
+player.width = img.player_guard:getWidth() ; player.height = img.player_guard:getHeight()
+player.speed = 10*60
