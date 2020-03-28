e87aadb3b65f05cbe34ebddcda94fc5f6a69c1a5
Sid-darth <realsiddarth@gmail.com>
2020-03-28 12:24:42 -0700

    added colliders for monsters
    *dumb update(no pathfinding yet)

--- monster.lua
+++ monster.lua
@@ -9,11 +9,14 @@ img.monster_3 = love.graphics.newImage("sprites/monster_3.png")
 local spawn_timer = 2
 local timer = spawn_timer
 
+
 function monsterUpdate(dt)
 	--move monster
 	for i,m1 in ipairs(monsters_1) do
-		m1.x = m1.x + math.cos(get_monsterAngle(m1)) *m1.speed*dt/2
-		m1.y = m1.y + math.sin(get_monsterAngle(m1)) *m1.speed*dt/2
+		-- m1.x = m1.x + math.cos(get_monsterAngle(m1)) *m1.speed*dt/2
+		-- m1.y = m1.y + math.sin(get_monsterAngle(m1)) *m1.speed*dt/2
+
+		m1.collider:setLinearVelocity(math.cos(get_monsterAngle(m1)) *(m1.speed*30)*dt/2,math.sin(get_monsterAngle(m1)) *(m1.speed*30)*dt/2)
 
 		for j,p in ipairs(projectiles) do	--detecting projectile hitting monster
 			if distance(p.x,p.y,m1.x,m1.y) < 20 then
@@ -53,6 +56,7 @@ end
 function spawn_monster()
 	local monster_1 = {}
 	monster_1.x = 0 ; monster_1.y = 0 ; monster_1.speed = player.speed*0.75
+	
 	monster_1.angle = get_monsterAngle(monster_1)
 	monster_1.width = img.monster_1:getWidth() ; monster_1.height = img.monster_1:getHeight()
 	monster_1.hit = false
@@ -67,12 +71,16 @@ function spawn_monster()
 	else
 		monster_1.x = -10 ; monster_1.y = math.random(0,window.height)
 	end
+	monster_1.collider = world:newCircleCollider(monster_1.x, monster_1.y, 20)
 
 	table.insert(monsters_1, monster_1)
 end
 
 function m1Draw()
+	world:draw()
 	for i,m1 in ipairs(monsters_1) do
+		m1.x = m1.collider:getX()
+		m1.y = m1.collider:getY()
 		love.graphics.draw(img.monster_1, m1.x, m1.y, get_monsterAngle(m1)+math.pi/2, 0.4, 0.4, m1.width/2, m1.height/2)
 	end
 end
